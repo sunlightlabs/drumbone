5bf3266e38f4898e34cb2631e2bc7c5c82d12fda
klondike <kprojection@gmail.com>
2010-01-29 18:38:18 -0500

    Now reading in actions and some relevant dates

--- models/bill.rb
+++ models/bill.rb
@@ -4,17 +4,25 @@ class Bill
   include MongoMapper::Document
   
   key :govtrack_id, String, :required => true
+  key :type, String, :required => true
+  key :code, String, :required => true
   key :chamber, String, :required => true
   key :session, String, :required => true
   key :state, String, :required => true
   
   ensure_index :govtrack_id
+  ensure_index :type
+  ensure_index :code
   ensure_index :chamber
   ensure_index :session
   ensure_index :introduced_at
   ensure_index :sponsor_id
   ensure_index :cosponsor_ids
   ensure_index :keywords
+  ensure_index :last_action_at
+  ensure_index :last_vote_at
+  ensure_index :signed_at
+  ensure_index :enacted_at
   
   timestamps!
   
@@ -29,9 +37,11 @@ class Bill
   
   def self.fields
     {
-      :basic => [:govtrack_id, :type, :code, :session, :chamber, :created_at, :updated_at, :state, :short_title, :official_title, :introduced_at],
+      :basic => [:govtrack_id, :type, :code, :session, :chamber, :created_at, :updated_at, :state],
+      :extended =>  [:short_title, :official_title, :introduced_at, :last_action_at, :last_vote_at, :signed_at, :enacted_at],
       :summary => [:summary],
       :keywords => [:keywords],
+      :actions => [:actions],
       :sponsorships => [:sponsor, :cosponsors],
       :sponsorship_ids => [:sponsor_id, :cosponsor_ids]
     }
@@ -45,62 +55,68 @@ class Bill
     start = Time.now
     
     FileUtils.mkdir_p "data/govtrack/#{session}/bills"
-    if system("rsync -az govtrack.us::govtrackdata/us/#{session}/bills/ data/govtrack/#{session}/bills/")
-      
-      bills = Dir.glob "data/govtrack/#{session}/bills/*.xml"
-      
-      # debug helpers
-      # bills = bills.first 20
-      # govtrack_id = "h3200"
-      # bills = bills.select {|b| b == "data/govtrack/111/bills/#{govtrack_id}.xml"}
-      
-      bills.each do |path|
-        doc = Hpricot open(path)
-        
-        type = doc.root.attributes['type']
-        number = doc.root.attributes['number']
-        govtrack_id = "#{type}#{number}"
-        
-        if bill = Bill.first(:conditions => {:govtrack_id => govtrack_id})
-          #puts "[Bill #{bill.govtrack_id}] About to be updated"
-        else
-          bill = Bill.new :govtrack_id => govtrack_id
-          #puts "[Bill #{bill.govtrack_id}] About to be created"
-        end
-        
-        sponsor = sponsor_for doc, missing_ids
-        cosponsors = cosponsors_for doc, missing_ids
-        
-        bill.attributes = {
-          :type => type,
-          :code => "#{code_for(type)}#{number}",
-          :session => session,
-          :chamber => chamber_for(type),
-          :state => doc.at(:state).inner_text,
-          :introduced_at => Time.at(doc.at(:introduced)['date'].to_i),
-          :short_title => short_title_for(doc),
-          :official_title => official_title_for(doc),
-          :keywords => doc.search('//subjects/term').map {|term| term['name']},
-          :summary => doc.at(:summary).inner_text,
-          :sponsor => sponsor,
-          :sponsor_id => sponsor ? sponsor[:govtrack_id] : nil,
-          :cosponsors => cosponsors,
-          :cosponsor_ids => cosponsors ? cosponsors.map {|c| c[:govtrack_id]} : nil
-        }
-        
-        bill.save
-        
-        count += 1
-      end
-      
-      Report.success self, "Synced #{count} bills for session ##{session} from GovTrack.us.", {:elapsed_time => Time.now - start}
-      
-      if missing_ids.any?
-        missing_ids = missing_ids.uniq
-        Report.warning self, "Found #{missing_ids.size} missing GovTrack IDs, attached.", {:missing_ids => missing_ids}
-      end
-    else
+    unless system("rsync -az govtrack.us::govtrackdata/us/#{session}/bills/ data/govtrack/#{session}/bills/")
       Report.failure self, "Couldn't rsync to Govtrack.us."
+      return
+    end
+      
+    bills = Dir.glob "data/govtrack/#{session}/bills/*.xml"
+    
+    # debug helpers
+    # bills = bills.first 20
+    # govtrack_id = "h2997"
+    # bills = bills.select {|b| b == "data/govtrack/111/bills/#{govtrack_id}.xml"}
+    
+    bills.each do |path|
+      doc = Hpricot::XML open(path)
+      
+      type = doc.root.attributes['type']
+      number = doc.root.attributes['number']
+      govtrack_id = "#{type}#{number}"
+      
+      if bill = Bill.first(:conditions => {:govtrack_id => govtrack_id})
+        # puts "[Bill #{bill.govtrack_id}] About to be updated"
+      else
+        bill = Bill.new :govtrack_id => govtrack_id
+        #puts "[Bill #{bill.govtrack_id}] About to be created"
+      end
+      
+      sponsor = sponsor_for doc, missing_ids
+      cosponsors = cosponsors_for doc, missing_ids
+      actions = actions_for doc
+      
+      bill.attributes = {
+        :type => type,
+        :code => "#{code_for(type)}#{number}",
+        :session => session,
+        :chamber => chamber_for(type),
+        :state => doc.at(:state).inner_text,
+        :introduced_at => Time.at(doc.at(:introduced)['date'].to_i),
+        :short_title => short_title_for(doc),
+        :official_title => official_title_for(doc),
+        :keywords => doc.search('//subjects/term').map {|term| term['name']},
+        :summary => doc.at(:summary).inner_text,
+        :sponsor => sponsor,
+        :sponsor_id => sponsor ? sponsor[:govtrack_id] : nil,
+        :cosponsors => cosponsors,
+        :cosponsor_ids => cosponsors ? cosponsors.map {|c| c[:govtrack_id]} : nil,
+        :actions => actions,
+        :last_action_at => actions ? actions.last[:acted_at] : nil,
+        :last_vote_at => last_vote_at_for(doc),
+        :enacted_at => enacted_at_for(doc),
+        :signed_at => signed_at_for(doc)
+      }
+      
+      bill.save
+      
+      count += 1
+    end
+    
+    Report.success self, "Synced #{count} bills for session ##{session} from GovTrack.us.", {:elapsed_time => Time.now - start}
+    
+    if missing_ids.any?
+      missing_ids = missing_ids.uniq
+      Report.warning self, "Found #{missing_ids.size} missing GovTrack IDs, attached.", {:missing_ids => missing_ids}
     end
   end
   
@@ -114,6 +130,45 @@ class Bill
       cosponsor and cosponsor['id'] ? legislator_for(cosponsor['id'], missing_ids) : nil
     end.compact
     cosponsors.any? ? cosponsors : nil
+  end
+  
+  def self.actions_for(doc)
+    actions = doc.search('//actions/*').reject {|a| a.class == Hpricot::Text}.map do |action|
+      {:acted_at => Time.at(action['date'].to_i),
+       :text => (action/:text).inner_text,
+       :type => action.name  
+       }
+    end
+    actions.any? ? actions : nil
+  end
+  
+  def self.enacted_at_for(doc)
+    if enacted = doc.at('//actions/enacted')
+      Time.at enacted['date'].to_i
+    end
+  end
+  
+  def self.signed_at_for(doc)
+    if signed = doc.at('//actions/signed')
+      Time.at signed['date'].to_i
+    end
+  end
+  
+  def self.last_vote_at_for(doc)
+    votes = doc.search '//actions/vote|//actions/vote-aux'
+    if votes.any?
+      Time.at votes.last['date'].to_i
+    end
+  end
+  
+  def self.short_title_for(doc)
+    titles = doc.search "//title[@type='short']"
+    titles.any? ? titles.last.inner_text : nil
+  end
+  
+  def self.official_title_for(doc)
+    titles = doc.search "//title[@type='official']"
+    titles.any? ? titles.last.inner_text : nil
   end
   
   def self.legislator_for(govtrack_id, missing_ids)
@@ -171,16 +226,6 @@ class Bill
   
   def self.format_time(time)
     time.strftime "%Y/%m/%d %H:%M:%S %z"
-  end
-  
-  def self.short_title_for(doc)
-    titles = doc.search "//title[@type='short']"
-    titles.any? ? titles.last.inner_text : nil
-  end
-  
-  def self.official_title_for(doc)
-    titles = doc.search "//title[@type='official']"
-    titles.any? ? titles.last.inner_text : nil
   end
   
   def self.current_session
