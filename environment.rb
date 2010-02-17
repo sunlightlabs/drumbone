98edab705988fbb5696a3b14f971d3767cc1734d
klondike <kprojection@gmail.com>
2010-02-17 15:21:35 -0500

    Sending email on warning and failure reports

--- environment.rb
+++ environment.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sunlight'
 require 'mongo_mapper'
+require 'pony'
 
 def config
   @config ||= YAML.load_file 'config/config.yml'
@@ -31,11 +32,13 @@ class Report
   
   timestamps!
   
+  
   def self.file(status, source, message, objects = {})
     report = Report.new :source => source.to_s, :status => status, :message => message
     report.attributes = objects
-    puts "[#{source}] #{status}: #{message}"
+    puts report.to_s
     report.save
+    report
   end
   
   def self.success(source, message, objects = {})
@@ -43,15 +46,37 @@ class Report
   end
   
   def self.failure(source, message, objects = {})
-    file 'FAILURE', source, message, objects
+    report = file 'FAILURE', source, message, objects
+    send_email report
   end
   
   def self.warning(source, message, objects = {})
-    file 'WARNING', source, message, objects
+    report = file 'WARNING', source, message, objects
+    send_email report
   end
   
   def self.latest(model, size = 1)
     reports = Report.all :conditions => {:source => model.to_s}, :order => "created_at DESC", :limit => size
     size > 1 ? reports : reports.first
   end
+  
+  def self.send_email(report)
+    Pony.mail email.merge(:subject => report.to_s, :body => report.attributes.inspect)
+  end
+  
+  def self.email=(details)
+    @email = details
+  end
+  
+  def self.email
+    @email
+  end
+  
+  def to_s
+    "[#{source}] #{status}: #{message}"
+  end
+end
+
+configure do
+  Report.email = config[:email]
 end
