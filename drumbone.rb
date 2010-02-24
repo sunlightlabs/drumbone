cc6dba8784213e8fb1042ebbcb0091a1504da78f
klondike <kprojection@gmail.com>
2010-02-24 11:25:04 -0500

    Put helper functions in a proper helpers block

--- drumbone.rb
+++ drumbone.rb
@@ -5,7 +5,6 @@ require 'sinatra'
 require 'environment'
 
 
-
 get /^\/(legislator)\.(json)$/ do
   fields = fields_for Legislator, params[:sections]
   conditions = conditions_for Legislator.unique_keys, params
@@ -74,43 +73,47 @@ get /^\/(rolls)\.(json)$/ do
 end
 
 
-def json(model, object, callback = nil)
-  response['Content-Type'] = 'application/json'
+helpers do
   
-  key = model.to_s.underscore
-  key = key.pluralize if object.is_a?(Array)
-  
-  json = {
-    key => object,
-    :sections => model.fields.keys.sort_by {|x, y| x == :basic ? -1 : x.to_s <=> y.to_s}
-  }.to_json
-  
-  callback ? "#{callback}(#{json});" : json
-end
-
-
-def conditions_for(keys, params)
-  conditions = {}
-  keys.each do |key|
-    conditions = conditions.merge(key => params[key]) if params[key]
+  def json(model, object, callback = nil)
+    response['Content-Type'] = 'application/json'
+    
+    key = model.to_s.underscore
+    key = key.pluralize if object.is_a?(Array)
+    
+    json = {
+      key => object,
+      :sections => model.fields.keys.sort_by {|x, y| x == :basic ? -1 : x.to_s <=> y.to_s}
+    }.to_json
+    
+    callback ? "#{callback}(#{json});" : json
   end
-  conditions
-end
 
-def pagination_for(params)
-  {
-    :limit => (params[:per_page] || 20).to_i,
-    :offset => ((params[:page] || 1).to_i - 1 ) * (params[:per_page] || 20).to_i
-  }
-end
 
-def fields_for(model, sections)
-  keys = sections ? (sections || '').split(',') : model.fields.keys
-  keys.uniq.map {|section| model.fields[section.to_sym]}.flatten.compact
-end
+  def conditions_for(keys, params)
+    conditions = {}
+    keys.each do |key|
+      conditions = conditions.merge(key => params[key]) if params[key]
+    end
+    conditions
+  end
 
-def attributes_for(document, fields)
-  attributes = document.attributes
-  attributes.keys.each {|key| attributes.delete(key) unless fields.include?(key.to_sym)}
-  attributes
+  def pagination_for(params)
+    {
+      :limit => (params[:per_page] || 20).to_i,
+      :offset => ((params[:page] || 1).to_i - 1 ) * (params[:per_page] || 20).to_i
+    }
+  end
+
+  def fields_for(model, sections)
+    keys = sections ? (sections || '').split(',') : model.fields.keys
+    keys.uniq.map {|section| model.fields[section.to_sym]}.flatten.compact
+  end
+
+  def attributes_for(document, fields)
+    attributes = document.attributes
+    attributes.keys.each {|key| attributes.delete(key) unless fields.include?(key.to_sym)}
+    attributes
+  end
+  
 end
