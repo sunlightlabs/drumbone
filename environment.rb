afd63b23ffd4e3a03d04dbf3329f6ad59e8344d7
Eric Mill <kprojection@gmail.com>
2010-11-22 11:30:54 -0500

    Updated reporting to only store missing govtrack ids with filename if it's 0, for reporting to GovTrack

--- environment.rb
+++ environment.rb
@@ -2,9 +2,11 @@ require 'rubygems'
 require 'sinatra'
 require 'sunlight'
 
+gem 'pony', '= 0.6'
 gem 'activesupport', '= 2.3.5'
 gem 'mongo', '=1.0.7'
 gem 'mongo_ext', '= 0.19.3'
+gem 'plucky', '= 0.3.3'
 gem 'mongo_mapper', '= 0.8.3'
 
 require 'active_support' 
@@ -21,8 +23,6 @@ end
 configure do
   MongoMapper.connection = Mongo::Connection.new config[:database][:hostname]
   MongoMapper.database = config[:database][:database]
-  
-  set :public, '.'
 end
 
 
--- models/legislator.rb
+++ models/legislator.rb
@@ -57,25 +57,25 @@ class Legislator
     # go through the CSV
     parties = {}
     FasterCSV.foreach("data/partytime/partytime_dump_all.csv") do |row|
-      crp_ids = row[25]
+      crp_ids = row[26]
       next unless crp_ids.present?
       
       crp_ids = crp_ids.split "||"
       
       # only select parties newer than 3 months ago
-      timestamp = "#{row[4]} #{row[6]}"
+      timestamp = "#{row[5]} #{row[6]}"
       if Time.parse(timestamp) > begin_days.days.ago
         crp_ids.each do |crp_id|
           parties[crp_id] ||= []
           
           parties[crp_id] << {
             :party_id => row[0],
-            :date => row[4],
-            :start_time => row[5],
-            :type => row[8],
-            :venue => row[9],
-            :venue_url => row[15],
-            :contribution_info => row[17]
+            :date => row[5],
+            :start_time => row[6],
+            :type => row[9],
+            :venue => row[10],
+            :venue_url => row[16],
+            :contribution_info => row[18]
           }
         end
       end
